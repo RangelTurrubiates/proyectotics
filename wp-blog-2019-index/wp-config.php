adc6abe66d1f35318f969ad38865452302b4e879
RangelTurrubiates <56322374+RangelTurrubiates@users.noreply.github.com>
2019-11-04 16:36:10 -0600

    agregado admin

--- wp-blog-2019-index/wp-config.php
+++ wp-blog-2019-index/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'wp-blog-2019-index' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'amdin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'e3bb4ae08590787ce4842a833d265257fa2aefe624fbe6f1' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
